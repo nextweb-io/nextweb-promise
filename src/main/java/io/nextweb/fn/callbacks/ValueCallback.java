84bac401f008a9d550b263bc28dd2027a4b1c94d
mxro <mxro@nowhere.com>
2014-02-19 15:48:16 +1100

    Reworking Appjangle cloud login for easier testing in order to fix bug with registration Autocommit Wed Feb 19 15:48:16 EST 2014

--- src/main/java/io/nextweb/fn/callbacks/ValueCallback.java
+++ src/main/java/io/nextweb/fn/callbacks/ValueCallback.java
@@ -1,7 +1,11 @@
 package io.nextweb.fn.callbacks;
 
 public interface ValueCallback<T> {
-
+	
+	/**
+	 * Asynchronous operation successfully completed.
+	 * @param value
+	 */
 	public void onSuccess(T value);
 	
 	public void onFailure(Throwable t);
