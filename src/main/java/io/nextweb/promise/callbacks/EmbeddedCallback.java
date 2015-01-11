8088380e8a8d3f5059a41c9a5b65b64b62c47fe2
mxro <mxro@nowhere.com>
2015-01-12 08:11:43 +1100

    Merging Promise Implementations Autocommit Mon Jan 12 08:11:43 EST 2015

--- src/main/java/io/nextweb/promise/callbacks/EmbeddedCallback.java
+++ src/main/java/io/nextweb/promise/callbacks/EmbeddedCallback.java
@@ -1,109 +1,115 @@
 package io.nextweb.promise.callbacks;
 
-import io.nextweb.promise.exceptions.NextwebExceptionManager;
 import io.nextweb.promise.exceptions.ExceptionResult;
 import io.nextweb.promise.exceptions.ImpossibleResult;
+import io.nextweb.promise.exceptions.NextwebExceptionManager;
 import io.nextweb.promise.exceptions.UnauthorizedResult;
 import io.nextweb.promise.exceptions.UndefinedResult;
 
 public class EmbeddedCallback<ResultType> implements NextwebCallback<ResultType> {
 
-	private final NextwebCallback<Object> embeddedIn;
-	private final NextwebExceptionManager exceptionManager;
+    private final NextwebCallback<Object> embeddedIn;
+    private final NextwebExceptionManager exceptionManager;
 
-	@Override
-	public void onUndefined(UndefinedResult r) {
+    @Override
+    public void onUndefined(final UndefinedResult r) {
 
-		if (hasEagerUndefinedListener()) {
-			embeddedIn.onUndefined(r);
-			return;
-		}
+        if (hasEagerUndefinedListener()) {
+            embeddedIn.onUndefined(r);
+            return;
+        }
 
-		if (exceptionManager.canCatchUndefinedExceptions()) {
-			exceptionManager.onUndefined(r);
-			return;
-		}
+        if (exceptionManager.canCatchUndefinedExceptions()) {
+            exceptionManager.onUndefined(r);
+            return;
+        }
 
-		embeddedIn.onUndefined(r);
+        embeddedIn.onUndefined(r);
 
-	}
+    }
 
-	@Override
-	public void onFailure(ExceptionResult r) {
-		if (hasEagerFailureListener()) {
-			embeddedIn.onFailure(r);
-			return;
-		}
+    @Override
+    public void onFailure(final ExceptionResult r) {
+        if (hasEagerFailureListener()) {
+            embeddedIn.onFailure(r);
+            return;
+        }
 
-		if (exceptionManager.canCatchExceptions()) {
-			exceptionManager.onFailure(r);
-			return;
-		}
+        if (exceptionManager.canCatchExceptions()) {
+            exceptionManager.onFailure(r);
+            return;
+        }
 
-		embeddedIn.onFailure(r);
-	}
+        embeddedIn.onFailure(r);
+    }
 
-	@Override
-	public void onImpossible(ImpossibleResult ir) {
-		if (hasEagerImpossibleListener()) {
-			embeddedIn.onImpossible(ir);
-			return;
-		}
+    @Override
+    public void onImpossible(final ImpossibleResult ir) {
+        if (hasEagerImpossibleListener()) {
+            embeddedIn.onImpossible(ir);
+            return;
+        }
 
-		if (exceptionManager.canCatchImpossibe()) {
-			exceptionManager.onImpossible(ir);
-			return;
-		}
+        if (exceptionManager.canCatchImpossibe()) {
+            exceptionManager.onImpossible(ir);
+            return;
+        }
 
-		embeddedIn.onImpossible(ir);
-	}
+        embeddedIn.onImpossible(ir);
+    }
 
-	@Override
-	public void onUnauthorized(UnauthorizedResult r) {
-		if (hasEagerUnauthorizedListener()) {
-			embeddedIn.onUnauthorized(r);
-			return;
-		}
+    @Override
+    public void onUnauthorized(final UnauthorizedResult r) {
+        if (hasEagerUnauthorizedListener()) {
+            embeddedIn.onUnauthorized(r);
+            return;
+        }
 
-		if (exceptionManager.canCatchAuthorizationExceptions()) {
-			exceptionManager.onUnauthorized(r);
-			return;
-		}
+        if (exceptionManager.canCatchAuthorizationExceptions()) {
+            exceptionManager.onUnauthorized(r);
+            return;
+        }
 
-		embeddedIn.onUnauthorized(r);
-	}
+        embeddedIn.onUnauthorized(r);
+    }
 
-	@Override
-	public void onSuccess(ResultType result) {
-		embeddedIn.onSuccess(result);
-	}
+    @Override
+    public void onSuccess(final ResultType result) {
+        embeddedIn.onSuccess(result);
+    }
 
-	@Override
-	public boolean hasEagerImpossibleListener() {
+    @Override
+    public boolean hasEagerImpossibleListener() {
 
-		return embeddedIn.hasEagerImpossibleListener();
-	}
+        return embeddedIn.hasEagerImpossibleListener();
+    }
 
-	@Override
-	public boolean hasEagerFailureListener() {
-		return embeddedIn.hasEagerFailureListener();
-	}
+    @Override
+    public boolean hasEagerFailureListener() {
+        return embeddedIn.hasEagerFailureListener();
+    }
 
-	@Override
-	public boolean hasEagerUndefinedListener() {
-		return embeddedIn.hasEagerUndefinedListener();
-	}
+    @Override
+    public boolean hasEagerUndefinedListener() {
+        return embeddedIn.hasEagerUndefinedListener();
+    }
 
-	@Override
-	public boolean hasEagerUnauthorizedListener() {
-		return embeddedIn.hasEagerUnauthorizedListener();
-	}
+    @Override
+    public boolean hasEagerUnauthorizedListener() {
+        return embeddedIn.hasEagerUnauthorizedListener();
+    }
 
-	public EmbeddedCallback(NextwebCallback<Object> embeddedIn,
-			NextwebExceptionManager exceptionManager) {
-		super();
-		this.embeddedIn = embeddedIn;
-		this.exceptionManager = exceptionManager;
-	}
+    public EmbeddedCallback(final NextwebCallback<Object> embeddedIn, final NextwebExceptionManager exceptionManager) {
+        super();
+        this.embeddedIn = embeddedIn;
+        this.exceptionManager = exceptionManager;
+    }
+
+    @Override
+    public boolean hasEagerListeners() {
+
+        return hasEagerFailureListener() || hasEagerImpossibleListener() || hasEagerUnauthorizedListener()
+                || hasEagerUndefinedListener();
+    }
 
 }
