916221e09e13f56a679800d2671dd4a3ff790dd1
mxro <mxro@nowhere.com>
2015-01-11 12:04:22 +1100

    Moving promise implementation into seperate project Autocommit Sun Jan 11 12:04:22 EST 2015

--- src/main/java/io/nextweb/promise/NextwebPromise.java
+++ src/main/java/io/nextweb/promise/NextwebPromise.java
@@ -2,9 +2,7 @@ package io.nextweb.promise;
 
 import io.nextweb.promise.exceptions.AllInterceptor;
 
-
-
 public interface NextwebPromise<ReturnType> extends BasicPromise<ReturnType>,
-		AllInterceptor<NextwebPromise<ReturnType>> {
+        AllInterceptor<NextwebPromise<ReturnType>> {
 
 }
