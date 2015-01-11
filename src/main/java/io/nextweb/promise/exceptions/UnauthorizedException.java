50af91e3b209961357bdfede9562c835b37f7816
mxro <mxro@nowhere.com>
2015-01-11 15:14:45 +1100

    Merging Promise Implementations Autocommit Sun Jan 11 15:14:45 EST 2015

--- src/main/java/io/nextweb/promise/exceptions/UnauthorizedException.java
+++ src/main/java/io/nextweb/promise/exceptions/UnauthorizedException.java
@@ -1,5 +1,18 @@
 package io.nextweb.promise.exceptions;
 
-public class UnauthorizedException {
+public class UnauthorizedException extends Throwable {
+
+    private static final long serialVersionUID = 1L;
+
+    private final UnauthorizedResult result;
+
+    public UnauthorizedException(final UnauthorizedResult result) {
+        super();
+        this.result = result;
+    }
+
+    public UnauthorizedResult getResult() {
+        return result;
+    }
 
 }
