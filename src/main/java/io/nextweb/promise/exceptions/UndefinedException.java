e4a1dfdcae241456a4fa179ae3895476b1b5653c
mxro <mxro@nowhere.com>
2015-01-11 15:15:45 +1100

    Merging Promise Implementations Autocommit Sun Jan 11 15:15:45 EST 2015

--- src/main/java/io/nextweb/promise/exceptions/UndefinedException.java
+++ src/main/java/io/nextweb/promise/exceptions/UndefinedException.java
@@ -1,5 +1,20 @@
 package io.nextweb.promise.exceptions;
 
-public class UndefinedException {
+public class UndefinedException extends Throwable {
+
+    private static final long serialVersionUID = 1L;
+
+    private final UndefinedResult result;
+
+    public UndefinedException(final UndefinedResult result) {
+        super();
+        this.result = result;
+    }
+
+    public UndefinedResult getResult() {
+        return result;
+    }t;
+
+
 
 }
