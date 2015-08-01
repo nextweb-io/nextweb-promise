dfa25c9fbbe620cc94cd3788970d07931a49be20
mxro <mxro@nowhere.com>
2015-08-01 10:36:00 +1000

    Prepare setting up new server Autocommit Sat Aug 01 10:36:00 EST 2015

--- src/main/java/io/nextweb/promise/callbacks/NextwebCallback.java
+++ src/main/java/io/nextweb/promise/callbacks/NextwebCallback.java
@@ -24,6 +24,6 @@ public interface NextwebCallback<ResultType> extends NextwebFailureCallback {
 
     public NextwebExceptionManager getExceptionManager();
 
-    public NextwebCallback<ResultType> embed(Closure<ResultType> onSuccess);
+    public NextwebCallback<ResultType> chain(Closure<ResultType> onSuccess);
 
 }
