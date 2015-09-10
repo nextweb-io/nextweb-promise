fab82bdd111923e2c8ddce11c298719d735616bc
mxro <mxro@nowhere.com>
2015-09-10 10:03:12 +1000

    Implementing better monitoring Autocommit Thu Sep 10 10:03:12 EST 2015

--- src/main/java/io/nextweb/promise/NextwebOperation.java
+++ src/main/java/io/nextweb/promise/NextwebOperation.java
@@ -1,9 +1,13 @@
 package io.nextweb.promise;
 
+import delight.async.callbacks.ValueCallback;
+
 import io.nextweb.promise.callbacks.NextwebCallback;
 
 public interface NextwebOperation<ResultType> {
 
     public void apply(NextwebCallback<ResultType> callback);
+
+    public void apply(ValueCallback<ResultType> callback);
 
 }
