95d589675c4405bf02828de018d52f302cd75771
mxro <mxro@nowhere.com>
2015-10-14 16:02:40 +1100

    Configuring new ObjectHub servers and fixing bugs Autocommit Wed Oct 14 16:02:40 EST 2015

--- src/main/java/io/nextweb/promise/exceptions/DataExceptionManager.java
+++ src/main/java/io/nextweb/promise/exceptions/DataExceptionManager.java
@@ -80,7 +80,6 @@ public class DataExceptionManager
         if (ENABLE_LOG) {
             System.out.println(this + ": Trigger unauthorized. Has:\n" + "  authListener: " + this.authExceptionListener
                     + "\n" + "  exceptionListener: " + this.exceptionListener);
-
         }
 
         if (this.authExceptionListener != null) {
