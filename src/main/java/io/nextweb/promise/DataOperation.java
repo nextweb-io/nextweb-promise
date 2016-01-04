f428fedad52c77743c20410f83c021d4b1654ca8
mxro <mxro@nowhere.com>
2016-01-04 18:05:46 +1100

    Developing ObjectHub platform Autocommit Mon Jan 04 18:05:46 EST 2016

--- src/main/java/io/nextweb/promise/DataOperation.java
+++ src/main/java/io/nextweb/promise/DataOperation.java
@@ -2,6 +2,14 @@ package io.nextweb.promise;
 
 import io.nextweb.promise.callbacks.DataCallback;
 
+/**
+ * <p>
+ * Abstraction for a basic operation on data.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ * @param <ResultType>
+ */
 public interface DataOperation<ResultType> {
 
     public void apply(DataCallback<ResultType> callback);
