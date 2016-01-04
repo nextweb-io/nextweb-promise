88b7e886e20ec22130f22bde320900f7d04ef301
mxro <mxro@nowhere.com>
2016-01-04 18:09:46 +1100

    Developing ObjectHub platform Autocommit Mon Jan 04 18:09:46 EST 2016

--- src/main/java/io/nextweb/promise/DataOperation.java
+++ src/main/java/io/nextweb/promise/DataOperation.java
@@ -6,7 +6,8 @@ import io.nextweb.promise.callbacks.DataCallback;
  * <p>
  * Abstraction for a basic operation on data.
  * <p>
- * In difference to a
+ * In difference to a {@link BasicPromise}, this operation supports additional
+ * callback methods specified in {@link DataCallback}.
  * 
  * @author <a href="http://www.mxro.de">Max Rohde</a>
  *
@@ -14,6 +15,14 @@ import io.nextweb.promise.callbacks.DataCallback;
  */
 public interface DataOperation<ResultType> {
 
+    /**
+     * <p>
+     * Called to perform this operation with the specified callback.
+     * 
+     * @param callback
+     *            The callback to be called once the operation has been
+     *            completed.
+     */
     public void apply(DataCallback<ResultType> callback);
 
 }
