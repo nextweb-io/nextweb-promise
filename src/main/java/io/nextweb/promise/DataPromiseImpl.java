db043bd385a1d031e3cb1afe5b0adc0124357995
mxro <mxro@nowhere.com>
2016-01-11 15:44:42 +1100

    Developing ObjectHub platform Autocommit Mon Jan 11 15:44:42 EST 2016

--- src/main/java/io/nextweb/promise/DataPromiseImpl.java
+++ src/main/java/io/nextweb/promise/DataPromiseImpl.java
@@ -113,14 +113,6 @@ public class DataPromiseImpl<ResultType> implements DataPromise<ResultType> {
         this.promise = promise;
         this.exceptionManager = exceptionManager;
 
-        promise.addExceptionFallback(new Closure<Throwable>() {
-
-            @Override
-            public void apply(final Throwable o) {
-                exceptionManager.onFailure(Fn.exception(this, o));
-            }
-        });
-
     }
 
     @Override
