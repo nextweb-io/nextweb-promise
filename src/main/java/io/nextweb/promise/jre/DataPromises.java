3135710ab6dd4d9227725b0316d5fdd04c806ede
mxro <mxro@nowhere.com>
2016-01-11 12:38:42 +1100

    Developing ObjectHub platform Autocommit Mon Jan 11 12:38:42 EST 2016

--- src/main/java/io/nextweb/promise/jre/DataPromises.java
+++ src/main/java/io/nextweb/promise/jre/DataPromises.java
@@ -2,14 +2,12 @@ package io.nextweb.promise.jre;
 
 import delight.async.Operation;
 import delight.async.callbacks.ValueCallback;
-import delight.functional.Closure;
 import delight.promise.Promise;
 import delight.promise.jre.Promises;
 
 import io.nextweb.promise.DataOperation;
 import io.nextweb.promise.DataPromise;
 import io.nextweb.promise.DataPromiseImpl;
-import io.nextweb.promise.Fn;
 import io.nextweb.promise.callbacks.DataCallback;
 import io.nextweb.promise.exceptions.DataExceptionManager;
 import io.nextweb.promise.utils.CallbackUtils;
@@ -36,14 +34,6 @@ public final class DataPromises {
 
                 operation.apply(dataCallback);
 
-            }
-        });
-
-        promise.addExceptionFallback(new Closure<Throwable>() {
-
-            @Override
-            public void apply(final Throwable o) {
-                exceptionManager.onFailure(Fn.exception(this, o));
             }
         });
 
